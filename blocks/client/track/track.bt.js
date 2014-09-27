519f8a6dd489086b536bd405d853ea8d579bda30
Morozov Andrew <anmorozov23@yandex.ru>
2014-09-27 19:33:53 +0400

    track: options bug fix

--- blocks/client/track/track.bt.js
+++ blocks/client/track/track.bt.js
@@ -2,20 +2,15 @@ module.exports = function (bt) {
     bt.match('track*', function (ctx) {
         ctx.enableAutoInit();
 
+        ctx.setInitOption('src', ctx.getParam('src'));
+
         ctx.setContent([{
             elem: 'text',
             content:  ctx.getParam('name')
-        }, {
-            elem: 'audio',
-            src: ctx.getParam('src')
         }]);
     });
 
     bt.match('track*__text', function (ctx) {
         ctx.setContent(ctx.getParam('content'));
-    });
-
-    bt.match('track*__audio', function (ctx) {
-        ctx.setInitOption('src', ctx.getParam('src'));
     });
 };
